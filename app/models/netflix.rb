c4edac3cbe7a0b3380c63a3ca134abceb9b9163c
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-02-11 15:54:40 -0800

    added movies and shows

--- app/models/netflix.rb
+++ app/models/netflix.rb
@@ -3,6 +3,9 @@ class Netflix < ApplicationRecord
         all = Netflix.all
         series = all.select{|netflix| netflix.vtype == 'series'}
         sorted_series = series.sort_by{|show| show.rating}.reverse
+        sorted_series.each do |show|
+            Show.create(title: show['title'], synopsis: show['synopsis'], vtype: show['vtype'], image: show['image'], year: show['year'], genre: show['genre'], rating: show['rating'], imdbrated: show['imdbrated'])
+        end
         return sorted_series[0]
     end
 
@@ -10,6 +13,9 @@ class Netflix < ApplicationRecord
         all = Netflix.all
         movies = all.select{|netflix| netflix.vtype == 'movie'}
         sorted_movies = movies.sort_by{|movie| movie.rating }.reverse
+        sorted_movies.each do |movie|
+            Movie.create(title: movie['title'], synopsis: movie['synopsis'], vtype: movie['vtype'], image: movie['image'], year: movie['year'], genre: movie['genre'], rating: movie['rating'], imdbrated: movie['imdbrated'])
+        end
         return sorted_movies[0]
     end
 
