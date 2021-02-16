2d6c3597f9858a6677fb20d3185a30c8ec43b784
dwisecar <dwisecar@gmail.com>
2021-02-16 14:39:03 -0800

    added create to likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,13 @@
 class LikesController < ApplicationController
-    
+  skip_before_action :authorized, only: [:create]
+  
+  def create
+    media_type = params["media_type"] == "movies" ? "Movie" : "Show"
+    like = {
+      user_id: params["user_id"],
+      media_id: params["media_id"],
+      media_type: media_type
+    }
+    Like.create(like)
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :medias
+  attributes :username, :id, :medias
 end
