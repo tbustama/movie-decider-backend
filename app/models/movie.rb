27dde9d0dbb98df9d34fe88e1a5102bc91733e20
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-02-17 14:41:31 -0800

    split and changed titles"

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,12 +3,21 @@ class Movie < ApplicationRecord
     has_many :users, through: :likes
 
     def self.comma
-        shows = Movie.all
-        shows.each{|show| show.update(synopsis: show.split_str)}
+        movies = Movie.all
+        movies.each{|movie| movie.update(synopsis: movie.split_str)}
+        movies.each{|movie| movie.update(title: movie.split_title)}
     end
 
     def split_str
         arr = self.synopsis.split(' ')
+        newArr = arr.map do |word|
+            word.gsub('&#39;', "'")
+        end
+        newArr.join(' ')
+    end
+
+    def split_title
+        arr = self.title.split(' ')
         newArr = arr.map do |word|
             word.gsub('&#39;', "'")
         end
--- app/models/show.rb
+++ app/models/show.rb
@@ -5,10 +5,20 @@ class Show < ApplicationRecord
     def self.comma
         shows = Show.all
         shows.each{|show| show.update(synopsis: show.split_str)}
+        shows.each{|show| show.update(title: show.split_title)}
     end
 
     def split_str
         arr = self.synopsis.split(' ')
+        newArr = arr.map do |word|
+            word.gsub('&#39;', "'")
+        end
+        newArr.join(' ')
+    end
+
+
+    def split_title
+        arr = self.title.split(' ')
         newArr = arr.map do |word|
             word.gsub('&#39;', "'")
         end
