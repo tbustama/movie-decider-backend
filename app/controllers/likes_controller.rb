2f69ef47e6cad47ff1a26f75791f1157e634c9fc
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-02-16 16:37:16 -0800

    created a custom route to delete a like"

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,4 +10,11 @@ class LikesController < ApplicationController
     }
     Like.create(like)
   end
+
+  def delete_like
+    media_type = params["media_type"] == "movies" ? "Movie" : "Show"
+    @like = Like.find_by(user_id: params[:user_id], media_id: params[:media_id], media_type: media_type)
+    @like.destroy
+  end
+
 end
