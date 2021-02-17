cb3ecb3f80cc6d32ce4594cb88a30447ac48d9f8
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-02-17 11:21:49 -0800

    added in methods to repalce synopsis

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,17 @@
 class Movie < ApplicationRecord
     has_many :likes, as: :movie
     has_many :users, through: :likes
+
+    def self.comma
+        shows = Movie.all
+        shows.each{|show| show.update(synopsis: show.split_str)}
+    end
+
+    def split_str
+        arr = self.synopsis.split(' ')
+        newArr = arr.map do |word|
+            word.gsub('&#39;', "'")
+        end
+        newArr.join(' ')
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,17 @@
 class Show < ApplicationRecord
     has_many :likes, :as => :show
     has_many :users, through: :likes
+    
+    def self.comma
+        shows = Show.all
+        shows.each{|show| show.update(synopsis: show.split_str)}
+    end
+
+    def split_str
+        arr = self.synopsis.split(' ')
+        newArr = arr.map do |word|
+            word.gsub('&#39;', "'")
+        end
+        newArr.join(' ')
+    end
 end
